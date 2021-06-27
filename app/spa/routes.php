7c26a962e22302de47a72e8a275e8ec6efe3321f
Jan F Kowalski <janfkowalski@gmail.com>
2021-06-27 16:37:25 +0200

    Initial Application.

---
+++ app/spa/routes.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Views\PhpRenderer;
+use App\Uniphpant\Actions\RequestAction;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->group('/', function (Group $group) {
+        $group->get('', RequestAction::class)
+            ->setName("page-request");
+    });
+};
---
+++ src/Uniphpant/Actions/RequestAction.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Uniphpant\Actions;
+
+use App\Application\Actions\Action;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Log\LoggerInterface;
+use Slim\Views\PhpRenderer;
+
+class RequestAction extends Action
+{
+    protected $renderer;
+
+    public function __construct(
+        LoggerInterface $logger,
+        PhpRenderer $renderer
+    )
+    {
+        parent::__construct($logger);
+        $this->renderer = $renderer;
+    }
+
+    protected function action(): Response
+    {
+        $templatesPath = __DIR__.'/../../../templates';
+        $this->renderer->setTemplatePath($templatesPath);
+
+        return $this->renderer->render(
+            $this->response,
+            "default.php",
+            [
+                'display_text'=>'Hello World!'
+            ]
+        );
+    }
+}
