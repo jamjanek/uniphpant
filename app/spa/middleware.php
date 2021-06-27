37313d08fb3d3525baa72775d06bf78f8a98c98d
Jan F Kowalski <janfkowalski@gmail.com>
2021-06-27 19:32:05 +0200

    [PR] middleware for the jamjan-ThinkPad-X250 attribute.

--- app/spa/middleware.php
+++ app/spa/middleware.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 use App\Application\Middleware\SessionMiddleware;
 use App\Uniphpant\Request\Middleware\HostCurrentMiddleware;
+use App\Uniphpant\Request\Middleware\HostNameMiddleware;
 use Slim\App;
 
 return function (App $app) {
     $app->add(SessionMiddleware::class);
+    $app->add(HostNameMiddleware::class);
     $app->add(HostCurrentMiddleware::class);
 };
---
+++ src/Uniphpant/Request/Middleware/HostNameMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Uniphpant\Request\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Laminas\Uri\UriFactory;
+
+/**
+ * Determine Hostname for the current Request
+ * Get Uri from Laminas\Uri\UriFactory and compose `scheme://host[:port]` string and set it as Requests attribute `hostname`.
+ * Attribute: hostname
+ */
+
+/**
+ * Class HostNameMiddleware
+ * @package App\Uniphpant\Request\Middleware
+ */
+class HostNameMiddleware implements Middleware
+{
+    protected $attribute_name = "hostname";
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        if(PHP_SAPI !== 'cli') {
+            $uri = UriFactory::factory((string)$request->getUri());
+
+            if (filter_var($uri->getHost(), FILTER_VALIDATE_IP)) {
+                $host = sprintf("%s://%s:%s", $uri->getScheme(), $uri->getHost(), $uri->getPort());
+            } else {
+                $host = sprintf("%s://%s", $uri->getScheme(), $uri->getHost());
+            }
+
+            $request = $request->withAttribute($this->attribute_name, $host);
+        }
+
+        return $handler->handle($request);
+    }
+}
