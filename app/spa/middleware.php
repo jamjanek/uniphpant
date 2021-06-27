fba600bf4c71ac8f8fdf12519e56351f00c2ea98
Jan F Kowalski <janfkowalski@gmail.com>
2021-06-27 20:34:53 +0200

    [PR] fixed attrname to constant. added default-site;

--- app/spa/middleware.php
+++ app/spa/middleware.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 use App\Application\Middleware\SessionMiddleware;
 use App\Uniphpant\Request\Middleware\HostCurrentMiddleware;
 use App\Uniphpant\Request\Middleware\HostNameMiddleware;
+use App\Uniphpant\Route\Middleware\FetchRouteMiddleware;
+use App\Uniphpant\Site\Middleware\SiteIdMiddleware;
 use Slim\App;
 
 return function (App $app) {
     $app->add(SessionMiddleware::class);
+    $app->add(FetchRouteMiddleware::class);
+    $app->add(SiteIdMiddleware::class);
     $app->add(HostNameMiddleware::class);
     $app->add(HostCurrentMiddleware::class);
 };
